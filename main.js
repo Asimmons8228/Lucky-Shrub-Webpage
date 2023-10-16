0e76f47e9bea3accccaf9bd13a3cf75aa45b63ac
Asimmons8228 <macbook@Macbooks-MBP.lan>
2023-10-16 18:41:07 -0400

    init commit

--- main.js
+++ main.js
@@ -1,49 +1,50 @@
 const logo = document.getElementById("logo");
 const navButtons = document.querySelectorAll(".navbuttons");
 
-// When you hover over the logo, show the navigation buttons
 logo.addEventListener("mouseenter", function () {
-    navButtons.forEach(function (button) {
-        button.style.opacity = 1;
-        button.style.pointerEvents = "auto";
-    });
-    if (!buttonsDisplayed) {
-        logo.classList.add("no-spin");
-        buttonsDisplayed = true;
-    }
+  navButtons.forEach(function (button) {
+    button.style.opacity = 1;
+    button.style.pointerEvents = "auto";
+  });
+  if (!buttonsDisplayed) {
+    logo.classList.add("no-spin");
+    buttonsDisplayed = true;
+  }
 });
 
-// When you move the cursor away from the logo, keep the navigation buttons displayed
 logo.addEventListener("mouseleave", function () {
-    navButtons.forEach(function (button) {
-        button.style.opacity = 1;
-        button.style.pointerEvents = "auto";
-    });
+  navButtons.forEach(function (button) {
+    button.style.opacity = 1;
+    button.style.pointerEvents = "auto";
+  });
 });
 
 var slideIndex = 1;
 showDivs(slideIndex);
 
 function plusDivs(n) {
-  showDivs(slideIndex += n);
+  showDivs((slideIndex += n));
 }
 
 function showDivs(n) {
   var i;
   var x = document.getElementsByClassName("slides");
-  if (n > x.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = x.length} ;
+  if (n > x.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = x.length;
+  }
   for (i = 0; i < x.length; i++) {
     x[i].style.display = "none";
   }
-  x[slideIndex-1].style.display = "block";
+  x[slideIndex - 1].style.display = "block";
 }
 function startSlideshow() {
-    setTimeout(function() {
-      plusDivs(1); // Advance to the next slide
-      startSlideshow(); // Call this function again for the next slide
-    }, 4000); // 1-second interval
-  }
-  
+  setTimeout(function () {
+    plusDivs(1);
+    startSlideshow();
+  }, 4000);
+}
 
-  startSlideshow();
+startSlideshow();
